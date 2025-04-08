518ab2fcca98e8dc6f185391d8cb335750f6b330
VitthalKokate <165715098+VitthalKokate@users.noreply.github.com>
2025-04-08 20:23:48 +0530

    AI_Project

---
+++ Ai_ImageGenrator/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './App.css'
+import { ImageGenrator } from './Componenets/ImageGenrator/ImageGenrator'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <ImageGenrator />
+        <br />
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ Ai_ImageGenrator/src/Componenets/ImageGenrator/ImageGenrator.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import './ImageGenrator.css'
+import default_Image from "../Assets/default_Img.jfif"
+import { useState } from 'react'
+import { useRef } from 'react'
+
+export const ImageGenrator = () => {
+
+  const imageGenrator = async () => {
+    if (inputRef.current.value === "") {
+      alert("Please enter a description!");
+      return;
+    }
+    const query = inputRef.current.value;
+    const response = await fetch(
+      `https://api.unsplash.com/photos/random?query=${query}&client_id=af2E5qlPY3v0ZEeo5jyBRulGL-QuOY4qo0GQcicGfaY`
+    );
+
+    if (!response.ok) {
+      console.error("Failed to fetch image");
+      return;
+    }
+
+    const data = await response.json();
+    setImage_Url(data.urls?.regular || "/");
+  };
+
+  const [image_Url, setImage_Url] = useState("/");
+  let inputRef = useRef(null);
+
+  return (
+    <div className='ai-image-genrator'>
+      <div className="header">AI Image <span> Generator</span></div>
+      <div className="img-loading">
+        <div className="image">
+          <img   src={image_Url === "/" ? default_Image : image_Url} alt='img' className='mx-auto mb-5  w-auto h-auto' />
+        </div>
+        <div className="search-box">
+          <input type="text" ref={inputRef} className='search-input' placeholder='Describe What You Want to See' />
+          <button className="generate-btn" onClick={imageGenrator}>Generate</button>
+        </div>
+      </div>
+    </div>
+  );
+};
